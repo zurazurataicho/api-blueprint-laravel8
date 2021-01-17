86c4e4d8cedf1a4e65e70dbd43cf5b4c6a399961
EZURA, Atsushi <zurazurataicho@gmail.com>
2021-01-17 18:02:53 +0900

    [change] port number for the local server

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:8000,127.0.0.1,127.0.0.1:8000,::1'
+        'localhost,localhost:8080,127.0.0.1,127.0.0.1:8080,::1'
     )),
 
     /*
