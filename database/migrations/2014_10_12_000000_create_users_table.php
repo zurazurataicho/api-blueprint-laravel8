4ed43c72eeaab129ab50b97937afafa02294caee
EZURA, Atsushi <zurazurataicho@gmail.com>
2021-01-26 14:19:12 +0900

    [modify] using nullable() for "not null" constraint

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable(false);
+            $table->string('email')->unique()->nullable(false);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable(false);
             $table->rememberToken();
             $table->timestamps();
+            $table->index(['email', 'password']);
         });
     }
 
